e55385c14772f09aa3471718a62ce659d043998d
Edouglas93 <58180699+Edouglas93@users.noreply.github.com>
2021-07-25 15:11:37 -0700

    functions

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ const misses = 'images/lostHeart.png';
 
 let missed = 0;
 
+//listen for the start game button to be pressed 
+  startButton.addEventListener('click', (e) => {
+    const overLay = document.querySelector('#overlay')
+    overLay.style.display = 'none';
+  });
+
+
 //arrays
 const gameSongPhrases = [
     'It Was A Good Day',
@@ -35,10 +42,10 @@ const addPhraseToDisplay = arr => {
     const phraseUl = document.querySelector('#phrase ul');
     phraseUl.appendChild(li);
     
-    if ( li.textContent == '') {
-        li.style.classList.add = '.space';
+    if ( li.textContent === '') {
+        li.classList.add('space');
     } else {
-        li.style.classList.add = '.letter';
+        li.classList.add("letter");
     }
   }  
 }
@@ -48,42 +55,34 @@ addPhraseToDisplay(phrasesToLettersArr);
 // checks if a letter is in the phrase
 const checkLetter = (button) => {
     let matched = null;
-    for (i=0; i < letters.length; i++) {
-      if (button === letters[i].textContent.toLowerCase()) {
-        letters[i].classList.add('show');
+    for (i=0; i < letter.length; i++) {
+      if (button === letter[i].textContent.toLowerCase()) {
+        letter[i].classList.add('show');
         matched = true;
       }
     }
     return matched;
   }
 
-  //check if the game has been won or lost
-  const checkWin = () => {
-      
-  }
-
-
-  //listen for the start game button to be pressed 
-  startButton.addEventListener('click', (e) => {
-    const overLay = document.querySelector('#overlay')
-    overLay.style.display = 'none';
-  });
-
-
   // listen for 
-  keyboard.addEventListener('click', e => {
+  qwerty.addEventListener('click', e => {
     if (e.target.tagName === "BUTTON") {
       e.target.className = 'chosen';
       e.target.disabled = true;
       const match = checkLetter(e.target.textContent.toLowerCase());
       if (match === null) {
         missed++;
-        // code to change heart icon from liveHeart.png to lostHeart.png would go here
+        tries.style.className = 'misses';
         misses.textContent = missed;
       }
       // checkWin() function call would go here
     }
   });
+
+    //check if the game has been won or lost
+    const checkWin = () => {
+      
+    }
 
 
 

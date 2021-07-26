a1c2d66349b15b9bca59cc433dbb43f9ea635aa4
Edouglas93 <58180699+Edouglas93@users.noreply.github.com>
2021-07-25 18:36:11 -0700

    update on functions

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 const keyboard = document.querySelector('#qwerty');
 const phrase = document.querySelector('#phrase');
 const startButton = document.querySelector('.btn__reset');
-const tries = document.querySelectorAll('.tries');
-const misses = 'images/lostHeart.png';
+const overLay = document.querySelector('#overlay');
+const misses = document.querySelectorAll('.tries') ;
 
 
 let missed = 0;
 
 //listen for the start game button to be pressed 
   startButton.addEventListener('click', (e) => {
-    const overLay = document.querySelector('#overlay')
-    overLay.style.display = 'none';
+      overLay.style.display = 'none';
   });
 
 
-//arrays
+//array on song names 
 const gameSongPhrases = [
     'It Was A Good Day',
     'Rapstar',
@@ -45,7 +44,7 @@ const addPhraseToDisplay = arr => {
     if ( li.textContent === '') {
         li.classList.add('space');
     } else {
-        li.classList.add("letter");
+        li.classList.add('letter');
     }
   }  
 }
@@ -55,33 +54,51 @@ addPhraseToDisplay(phrasesToLettersArr);
 // checks if a letter is in the phrase
 const checkLetter = (button) => {
     let matched = null;
-    for (i=0; i < letter.length; i++) {
-      if (button === letter[i].textContent.toLowerCase()) {
-        letter[i].classList.add('show');
+    let letters = document.querySelectorAll('.letter')
+    for (i=0; i < letters.length; i++) {
+      if (button === letters[i].textContent.toLowerCase()) {
+        letters[i].classList.add('show');
         matched = true;
       }
     }
     return matched;
   }
 
+  const hearts = document.querySelector('.tries img');
+
   // listen for 
-  qwerty.addEventListener('click', e => {
+  qwerty.addEventListener('click', (e) => {
     if (e.target.tagName === "BUTTON") {
       e.target.className = 'chosen';
       e.target.disabled = true;
       const match = checkLetter(e.target.textContent.toLowerCase());
       if (match === null) {
+        hearts.src = 'images/lostHeart.png';
         missed++;
-        tries.style.className = 'misses';
-        misses.textContent = missed;
       }
-      // checkWin() function call would go here
-    }
+
+      checkWin()
+      }
   });
 
     //check if the game has been won or lost
     const checkWin = () => {
-      
+      let letters = document.querySelectorAll('.letter')
+      const title = document.querySelectorAll('.title');
+      const show = document.querySelectorAll('.show');
+      if (letters.length === show.length) {
+        overlay.classList.add('win')
+        title.textContent = 'Winner Winner Chicken Dinner!';
+        overLay.style.display = 'flex';
+      }
+      else {
+        if (missed > 4) {
+          overlay.classList.add('lose')
+          title.textContent = 'You Are Out of Lives, Try Again!';
+          overLay.style.display = 'flex';         
+        }
+
+      }
     }
 
 
